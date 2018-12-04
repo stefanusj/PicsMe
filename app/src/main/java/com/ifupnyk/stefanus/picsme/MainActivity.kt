9610627424d8de3e203278b3ccee90bfd5a2a1e2
Stefanus Julianto <stefanusj3@gmail.com>
2018-12-04 12:07:53 +0700

    Initial commit

---
+++ app/src/main/java/com/ifupnyk/stefanus/picsme/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ifupnyk.stefanus.picsme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.setLogo(R.drawable.ic_edit)
+    }
+}
